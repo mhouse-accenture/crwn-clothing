70d7c6746633bdfa445e32bafa9a3444d09f89b6
MATTHEW HOUSE <M.HOUSE@ACCENTURE.COM>
2020-02-14 15:49:44 -0500

    Create sign in component and sign in/sign up page

---
+++ src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-in-sign-out.component';
+import SignIn from '../../components/sign-up/sign-up.component';
+
+const SignInSignUp = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+);
+
+export default SignInSignUp;
