c02547ef0ee66e9d837e26d165d98d49d4c86fd3
MATTHEW HOUSE <M.HOUSE@ACCENTURE.COM>
2020-02-17 11:51:28 -0500

    Fixing mixup of sign-in and sign-up components

--- src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
+++ src/pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './sign-in-sign-out.component';
-import SignIn from '../../components/sign-up/sign-up.component';
+import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInSignUp = () => (
   <div className='sign-in-and-sign-up'>
